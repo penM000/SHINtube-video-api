09d58f9cd6b1c4aa2996f3a9770c2f8227b03e6e
BuildTools <unconfigured@null.spigotmc.org>
2021-06-25 14:10:42 +0900

    エンコードのテスト

--- app/main.py
+++ app/main.py
@@ -1,22 +1,38 @@
-from fastapi import Depends, FastAPI
-
-from .internal import admin
-from .routers.apis import fullname, search, pageid
+from .internal.module.video.encode import encode
+import aiofiles
+from fastapi import FastAPI
+from fastapi import File, UploadFile
+from fastapi.staticfiles import StaticFiles
+from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-
-
-
-app.include_router(fullname.router)
-app.include_router(pageid.router)
-app.include_router(search.router)
-app.include_router(
-    admin.router,
-    prefix="",
-    tags=["admin"],
-    responses={418: {"description": "I'm a teapot"}},
+app.mount("/video", StaticFiles(directory="./video"), name="video")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,   # 追記により追加
+    allow_methods=["*"],      # 追記により追加
+    allow_headers=["*"]       # 追記により追加
 )
+
 
 @app.get("/")
 async def root():
     return {"message": "Hello Bigger Applications!"}
+
+
+@app.post("/upload")
+async def post_endpoint(in_file: UploadFile = File(...)):
+
+    async with aiofiles.open("./video/1.mp4", 'wb') as out_file:
+        while True:
+            # 書き込みサイズ(MB)
+            chunk = 4
+            content = await in_file.read(chunk * 1048576)  # async read chunk
+            if content:
+                await out_file.write(content)  # async write chunk
+            else:
+                break
+    await encode("./video", "1.mp4")
+
+    return {"Result": "OK"}
