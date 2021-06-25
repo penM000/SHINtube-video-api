fc8c43126bc4b42776e134c642536e4e8a2a7aab
BuildTools <unconfigured@null.spigotmc.org>
2021-06-25 14:17:30 +0900

    エンコード関数の追加

---
+++ app/internal/module/video/encode.py
@@ -0,0 +1,8 @@
+from ..command_run import command_run
+
+
+async def encode(folderpath: str, filename: str, width=1920, height=1080) -> bool:
+    temp = filename.split(".")[:-2]
+    await command_run("ls", "./bin/")
+    await command_run("chmod +x ffmpeg", "./bin/")
+    await command_run(f"./bin/ffmpeg -i {folderpath}/{filename} -codec: copy -start_number 0 -hls_time 10 -hls_list_size 0 -f hls {folderpath}/{filename}.m3u8 ", "./")
