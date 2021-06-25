bdbc16ae4090f2994d56d4b4da9aac4e1648e3dd
BuildTools <unconfigured@null.spigotmc.org>
2021-06-25 14:11:04 +0900

    階層の追加

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # スクリプトの場所に移動
 cd `dirname $0`
-pip3 install -U fastapi uvicorn aiohttp aiofiles
+pip3 install -U fastapi uvicorn aiohttp aiofiles python-multipart
 if [ "root" = `whoami` ]; then
   uvicorn app.main:app --reload --host 0.0.0.0 --port 8000 --log-level debug --proxy-headers
 else
