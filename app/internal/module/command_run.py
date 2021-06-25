dc4a21fc2bd058b98109415926edf09911d1a2c6
BuildTools <unconfigured@null.spigotmc.org>
2021-06-25 14:10:25 +0900

    非同期コマンド実行関数

---
+++ app/internal/module/command_run.py
@@ -0,0 +1,17 @@
+import asyncio
+
+
+async def command_run(cmd, cwd):
+    proc = await asyncio.create_subprocess_shell(
+        cmd,
+        cwd=cwd,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE
+    )
+    stdout, stderr = await proc.communicate()
+    print(f'[{cmd!r} exited with {proc.returncode}]')
+    if stdout:
+        print(f'[stdout]\n{stdout.decode()}')
+    if stderr:
+        print(f'[stderr]\n{stderr.decode()}')
+    return proc.returncode
