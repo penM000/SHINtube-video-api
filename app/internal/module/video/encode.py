886d40befc9fbacbbf01c92d714b8db0f206e419
BuildTools <unconfigured@null.spigotmc.org>
2021-06-25 14:41:02 +0900

    計算時間の追加

--- app/internal/module/video/encode.py
+++ app/internal/module/video/encode.py
@@ -1,4 +1,5 @@
 from ..command_run import command_run
+import time
 
 
 async def encode(folderpath: str, filename: str, width=1920, height=1080) -> bool:
@@ -9,7 +10,9 @@ async def encode(folderpath: str, filename: str, width=1920, height=1080) -> boo
     # await command_run(f"./bin/ffmpeg -i {folderpath}/{filename} -codec: copy
     # -start_number 0 -hls_time 10 -hls_list_size 0 -f hls
     # {folderpath}/{filename}.m3u8 ", "./")
+    now = time.time()
     await command_run(
         f"./bin/ffmpeg -i {folderpath}/{filename} \
             -crf 20 -start_number 0 -hls_time 10 \
                 -hls_list_size 0 -f hls {folderpath}/{filename}.m3u8 ", "./")
+    print(time.time() - now)
