7bf95da1b1403c112a153aba03e45c10afcf5469
BuildTools <unconfigured@null.spigotmc.org>
2021-06-26 18:38:28 +0900

    ffmpegの引数をスリムに

--- app/internal/module/video/encode.py
+++ app/internal/module/video/encode.py
@@ -3,16 +3,22 @@ import time
 
 
 async def encode(folderpath: str, filename: str, width=1920, height=1080) -> bool:
-    temp = filename.split(".")[:-2]
-    await command_run("ls", "./bin/")
+    temp = "".join(filename.split(".")[:-1])
     await command_run("chmod +x ffmpeg", "./bin/")
-
-    # await command_run(f"./bin/ffmpeg -i {folderpath}/{filename} -codec: copy
-    # -start_number 0 -hls_time 10 -hls_list_size 0 -f hls
-    # {folderpath}/{filename}.m3u8 ", "./")
     now = time.time()
-    await command_run(
-        f"./bin/ffmpeg -i {folderpath}/{filename} \
-            -crf 20 -start_number 0 -hls_time 10 \
-                -hls_list_size 0 -f hls {folderpath}/{filename}.m3u8 ", "./")
+    command = [
+        "./bin/ffmpeg",
+        "-threads 1",
+        f"-i {folderpath}/{filename}",
+        "-threads 1",
+        "-crf 20",
+        "-start_number 0",
+        "-hls_time 10",
+        "-hls_list_size 0",
+        "-f hls",
+        # f"-s {width}x{height}",
+        f"-vf scale=-1:{height}",
+        f"{folderpath}/{temp}_{height}p.m3u8"
+    ]
+    await command_run(" ".join(command), "./")
     print(time.time() - now)
