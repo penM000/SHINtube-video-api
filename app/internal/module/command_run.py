c5cc21a44444cbc01528fb7206e9ed5a21383c87
BuildTools <unconfigured@null.spigotmc.org>
2021-06-26 18:36:40 +0900

    リアルタイム表示に対応

--- app/internal/module/command_run.py
+++ app/internal/module/command_run.py
@@ -1,17 +1,23 @@
 import asyncio
 
 
-async def command_run(cmd, cwd):
+async def command_run(cmd, cwd, normal_mode=True):
     proc = await asyncio.create_subprocess_shell(
         cmd,
         cwd=cwd,
         stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE
+        stderr=asyncio.subprocess.STDOUT
     )
-    stdout, stderr = await proc.communicate()
-    print(f'[{cmd!r} exited with {proc.returncode}]')
-    if stdout:
-        print(f'[stdout]\n{stdout.decode()}')
-    if stderr:
-        print(f'[stderr]\n{stderr.decode()}')
+    if normal_mode:
+        stdout, stderr = await proc.communicate()
+        if stdout:
+            pass
+        # print(f'[stdout]\n{stdout.decode()}')
+        if stderr:
+            pass
+            # print(f'[stderr]\n{stderr.decode()}')
+    else:
+        while proc.returncode is None:
+            line = await proc.stderr.readline()
+            print(line.decode().replace("\n", ""))
     return proc.returncode
