3a787270afacfabf3455fb6042bc14182d83bee8
BuildTools <unconfigured@null.spigotmc.org>
2021-08-23 13:53:50 +0900

    dockerの追加

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # スクリプトの場所に移動
 cd `dirname $0`
+sudo apt update || apt update
+sudo apt install ffmpeg -y || apt install ffmpeg -y
 pip3 install -U fastapi uvicorn aiohttp aiofiles python-multipart aiopath
 if [ "root" = `whoami` ]; then
   uvicorn app.main:app --reload --host 0.0.0.0 --port 8000 --log-level debug --proxy-headers
