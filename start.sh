7f29719790a025496b68c85393a27aefb1ec5b3e
BuildTools <unconfigured@null.spigotmc.org>
2021-08-27 14:55:50 +0900

    コンテナ内がおかしくなたとき用

--- start.sh
+++ start.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 # スクリプトの場所に移動
 cd `dirname $0`
+sudo dpkg --configure -a || dpkg --configure -a
 sudo apt update || apt update
 sudo apt install ffmpeg -y || apt install ffmpeg -y
+sudo dpkg --configure -a || dpkg --configure -a
 pip3 install -U fastapi uvicorn aiohttp aiofiles python-multipart aiopath
 if [ "root" = `whoami` ]; then
-  uvicorn app.main:app  --host 0.0.0.0 --port 8000 --log-level debug --proxy-headers #--reload
+  uvicorn app.main:app  --host 0.0.0.0 --port 8000 --log-level debug --proxy-headers --reload
 else
-  ~/.local/bin/uvicorn app.main:app  --host 0.0.0.0 --port 8000 --log-level debug --proxy-headers  #--reload
+  ~/.local/bin/uvicorn app.main:app  --host 0.0.0.0 --port 8000 --log-level debug --proxy-headers  --reload
 fi
