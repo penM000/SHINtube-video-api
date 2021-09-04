0711b50f3cf45ad6b2dd3d8f3220c8d77bd8a53a
BuildTools <unconfigured@null.spigotmc.org>
2021-09-04 10:16:13 +0900

    nvencの用意

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ sudo dpkg --configure -a || dpkg --configure -a
 sudo apt update || apt update
 sudo apt install ffmpeg -y || apt install ffmpeg -y
 sudo dpkg --configure -a || dpkg --configure -a
-pip3 install -U fastapi uvicorn aiohttp aiofiles python-multipart aiopath
+pip3 install -U fastapi uvicorn[standard] aiohttp aiofiles python-multipart aiopath
 if [ "root" = `whoami` ]; then
   uvicorn app.main:app  --host 0.0.0.0 --port 8000 --log-level debug --proxy-headers --reload
 else
