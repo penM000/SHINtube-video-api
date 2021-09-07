febfaed4750e40dbe683e83528279029fc4cba0f
BuildTools <unconfigured@null.spigotmc.org>
2021-09-07 19:08:51 +0900

    リロード場所の明記

--- start.sh
+++ start.sh
@@ -5,9 +5,11 @@ sudo dpkg --configure -a || dpkg --configure -a
 sudo apt update || apt update
 sudo apt install ffmpeg -y || apt install ffmpeg -y
 sudo dpkg --configure -a || dpkg --configure -a
-pip3 install -U fastapi uvicorn[standard] aiohttp aiofiles python-multipart aiopath
+pip3 install -U fastapi uvicorn[standard] aiohttp aiofiles python-multipart aiopath 
+
+
 if [ "root" = `whoami` ]; then
-  uvicorn app.main:app  --host 0.0.0.0 --port 8000 --log-level debug --proxy-headers --reload
+  uvicorn app.main:app  --host 0.0.0.0 --port 8000 --log-level debug --proxy-headers --reload --reload-dir ./app
 else
-  ~/.local/bin/uvicorn app.main:app  --host 0.0.0.0 --port 8000 --log-level debug --proxy-headers  --reload
+  ~/.local/bin/uvicorn app.main:app  --host 0.0.0.0 --port 8000 --log-level debug --proxy-headers  --reload --reload-dir ./app
 fi
