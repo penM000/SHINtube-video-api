7207d74d633cd7763e473c03aa4b237c5352f0ca
BuildTools <unconfigured@null.spigotmc.org>
2021-12-02 19:13:27 +0900

    構文の修正

--- nginx/basic.sh
+++ nginx/basic.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if { [ "${BASIC_USER:+foo}" ] && [ "${BASIC_PASS:+foo}" ] }; then
+if [ "${BASIC_USER:+foo}" ] && [ "${BASIC_PASS:+foo}" ]; then
     echo BASIC認証用のキーを作成
     CRYPTPASS=`openssl passwd -crypt ${BASIC_PASS}`
     echo "${BASIC_USER}:${CRYPTPASS}" >> /etc/nginx/.htpasswd
