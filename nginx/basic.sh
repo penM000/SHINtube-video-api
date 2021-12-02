56b6cd7e3f529d666ab0647e4a031f34da6b6b88
BuildTools <unconfigured@null.spigotmc.org>
2021-12-02 19:10:26 +0900

    ベーシック認証に対応

---
+++ nginx/basic.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+if { [ "${BASIC_USER:+foo}" ] && [ "${BASIC_PASS:+foo}" ] }; then
+    echo BASIC認証用のキーを作成
+    CRYPTPASS=`openssl passwd -crypt ${BASIC_PASS}`
+    echo "${BASIC_USER}:${CRYPTPASS}" >> /etc/nginx/.htpasswd
+fi
